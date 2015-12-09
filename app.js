9fef8d7ae10332f2ecdebda6d4dd344150b0b844
dennerkrans <dennerkrans@gmail.com>
2015-12-09 19:58:00 +0100

    changed stuff

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = 3000;
 
 app.use(express.static(__dirname + '/'));
 
--- tilt.js
+++ tilt.js
@@ -1,17 +1,27 @@
-function scaleRange(inputValue, inputMin, inputMax, outputMin, outputMax){
-  return parseInt(inputValue / ((inputMax - inputMin) / (outputMax - outputMin)) + outputMin);
+function scaleRange(inputValue, inputMax){
+  return parseInt(inputValue / (inputMax / 255));
 }
 
+var o = document.getElementById('output');
+
+var handleOrientation = function (event) {
+  var z = scaleRange(Math.round(event.alpha), 360); // Direction of device, range [0, 360]
+  var x = Math.round(event.beta); // Tilt front-to-back, range [-180,180]
+  x += 180;
+  x = scaleRange(x, 360);
+  var y = Math.round(event.gamma); // Tilt left-to-right, range [-90,90]
+
+  console.log(z);
+  console.log(x);
+  console.log(y);
+
+  o.innerHTML = 'z: ' + Math.round(z) + '\n';
+  o.innerHTML += 'x: ' + Math.round(x) + '\n';
+  o.innerHTML +='y: ' + Math.round(y);
+
+  $('.color').css('background-color', 'rgb(' + z + ',' + x + ',' + y + ')');
+};
 
 if (window.DeviceOrientationEvent) {
-  window.addEventListener("deviceorientation", function handleOrientation(event) {
-    var z = event.alpha; // Direction of device, range [0, 360]
-    var x = event.beta; // Tilt front-to-back, range [-180,180]
-    var y = event.gamma; // Tilt left-to-right, range [-90,90]
-
-    console.log(z);
-    console.log(x);
-    console.log(y);
-
-  }, false);
+  window.addEventListener("deviceorientation", handleOrientation, false);
 }
