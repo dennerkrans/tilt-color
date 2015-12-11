431efce11c9112a1be0a864003f8637c58397f4b
dennerkrans <dennerkrans@gmail.com>
2015-12-11 10:20:45 +0100

    Added a map function to the Number prototype

--- tilt.js
+++ tilt.js
@@ -1,25 +1,28 @@
-function scaleRange(inputValue, inputMax){
-  return parseInt(inputValue / (inputMax / 255));
-}
+Number.prototype.map = function (in_min, in_max, out_min, out_max) {
+  return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+};
 
 var o = document.getElementById('output');
 
 var handleOrientation = function (event) {
-  var z = scaleRange(Math.round(event.alpha), 360); // Direction of device, range [0, 360]
-  var x = Math.round(event.beta); // Tilt front-to-back, range [-180,180]
-  x += 180;
-  x = scaleRange(x, 360);
-  var y = Math.round(event.gamma); // Tilt left-to-right, range [-90,90]
+  var z = event.alpha; // Direction of device, range [0, 360]
+  var x = event.beta; // Tilt front-to-back, range [-180,180]
+  var y = event.gamma; // Tilt left-to-right, range [-90,90]
 
-  console.log(z);
-  console.log(x);
-  console.log(y);
+  if (x < -90)
+    x = -90;
+  if (x > 90)
+    x = 90;
 
-  o.innerHTML = 'z: ' + Math.round(z) + '\n';
-  o.innerHTML += 'x: ' + Math.round(x) + '\n';
-  o.innerHTML +='y: ' + Math.round(y);
+  z = z.map(0, 360, 0, 255);
+  x = x.map(-90, 90, 0, 255);
+  y = y.map(-90, 90, 0, 255);
 
-  $('.color').css('background-color', 'rgb(' + z + ',' + x + ',' + y + ')');
+  // o.innerHTML = 'z: ' + z + '\n';
+  // o.innerHTML += 'x: ' + x + '\n';
+  // o.innerHTML +='y: ' + y;
+
+  $('body').css('background-color', 'rgb(' + Math.round(z) + ',' + Math.round(x) + ',' + Math.round(y) + ')');
 };
 
 if (window.DeviceOrientationEvent) {
