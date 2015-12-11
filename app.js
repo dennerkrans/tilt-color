de004d8982beeb79456d7563bc5ae80326fe11f9
dennerkrans <dennerkrans@gmail.com>
2015-12-11 16:54:41 +0100

    put index.html and tilt.js in public folder, added a logger and set so only the public folder is served to the client

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/'));
+var logger = require('./logger');
+app.use(logger);
+
+app.use(express.static('public'));
 
 io.on('connection', function(socket) {
   console.log('a user connected');
   socket.on('colors', function(color){
-    console.log('This is the color: ' + color);
+    // console.log('This is the color: ' + color);
     io.emit('colors', color);
   });
 });
